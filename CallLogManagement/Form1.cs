abf2374ed0c95b423d2c60e7d358959098369e99
jaseyaws <jcartin@ykw.systems>
2023-02-17 11:06:31 -0500

    start project

---
+++ CallLogManagement/Form1.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CallLogManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                    textBox2.Focus();
+                }
+                else
+                {
+                    textBox1.Focus();
+                }                
+            }
+        }
