e9c5124dbd240883475799f64984c76279b81cd9
jaseyaws <jcartin@ykw.systems>
2023-02-17 11:16:38 -0500

    completed form1 functionality

--- CallLogManagement/Form1.cs
+++ CallLogManagement/Form1.cs
@@ -61,3 +61,99 @@ namespace CallLogManagement
                 }
             }
         }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (comboBox1.SelectedIndex != -1)
+                {
+                    dateTimePicker1.Focus();
+                }
+                else
+                {
+                    comboBox1.Focus();
+                }
+            }
+        }
+
+        private void dateTimePicker1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox5.Focus();
+            }
+        }
+
+        private void textBox5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox5.Text.Length > 0)
+                {
+                    textBox6.Focus();
+                }
+                else
+                {
+                    textBox5.Focus();
+                }
+            }
+        }
+
+        private void textBox6_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox6.Text.Length > 0)
+                {
+                    textBox7.Focus();
+                }
+                else
+                {
+                    textBox6.Focus();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                errorProvider1.Clear();
+                errorProvider1.SetError(textBox1, "Enter Name");
+            }
+            else if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                errorProvider1.Clear();
+                errorProvider1.SetError(textBox3, "Enter Mobile No");
+            }
+            else
+            {
+                Connection cn = new Connection();
+                cn.dataSend(@"INSERT INTO CallDetails (Name, FatherName, Address, Mobile, Date, Time, Duration, Notes, Status)
+                            VALUES ('"+textBox1.Text+"','"+textBox2.Text+"','"+textBox4.Text+"','"+textBox3.Text+"','"+dateTimePicker1.Value.ToString()+"','"+textBox5.Text+"','"+textBox6.Text+"','"+textBox7.Text+"','"+comboBox1.Text+"')");
+                MessageBox.Show("Saved Successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Clear();
+            }
+        }
+        void Clear()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            comboBox1.SelectedIndex = -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CallLogManagement.ViewCallDetails VCD = new ViewCallDetails();
+            VCD.StartPosition = FormStartPosition.CenterParent;
+            VCD.Show();
+        }
+    }
+}
